644ad6a0e36cd9068ddd7e7c1016d7cc0047a69e
Anderson Lozano <adslozanov@gmail.com>
2025-03-19 13:20:46 -0500

    💄 The interface property is applied to load a custom message.

--- app/components/Loading.component.tsx
+++ app/components/Loading.component.tsx
@@ -1,7 +1,10 @@
-export function Loading() {
+import { LoadingProps } from "~/interfaces/Loading.interface";
+
+export function Loading(props: LoadingProps) {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6">
       <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-sky-vivid"></div>
+      <p className="ml-4 text-sky-vivid text-lg font-medium">{props.message}</p>
     </div>
   );
 }
