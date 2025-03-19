afaced9d8746ac3824deeb53794d9dfad0fbf9ac
Anderson Lozano <adslozanov@gmail.com>
2025-03-19 00:45:30 -0500

    🔨 Metadata for page positioning is added.

--- app/routes/questionnaire.tsx
+++ app/routes/questionnaire.tsx
@@ -1,4 +1,12 @@
+import type { MetaFunction } from "@remix-run/node";
 import { Questionnaire } from "../components/QuestionnaireScreen.component";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Amadeus - Questionnaire" },
+    { name: "description", content: "Aquí es donde selecionaras las preguntas que te ayudaran a escoger la mejor opcción de viaje para ti" },
+  ];
+};
 
 export default function QuestionnaireRoute() {
   return <Questionnaire />;
