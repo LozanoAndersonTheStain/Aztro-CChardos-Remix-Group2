bd6f6e80d5611a0a72e965f553ba58d52b1096ab
Anderson Lozano <adslozanov@gmail.com>
2025-03-18 19:35:15 -0500

    💄 Properties are added to the menu to make it fade in or out.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,37 @@
 import { Link, useLocation } from "@remix-run/react";
+import { useState } from "react";
 
 export default function Navbar() {
   const location = useLocation();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  // Si no estamos en la página de inicio, el navbar se desvanece
+  const isHomePage = location.pathname === "/";
+  const navbarOpacity = isHomePage || isHovered ? "opacity-100" : "opacity-0";
 
   return (
-    <nav className={`backdrop-blur-lg bg-black/10 text-white p-6 flex justify-between items-center fixed transition-all duration-5000 ease-in-out mx-4 md:mx-8 rounded-xl w-[calc(100%-2rem)] md:w-[calc(100%-4rem)] z-50 ${
-      location.pathname === "/" ? "top-6" : "-top-24"
-    }`}>
-      <img src="/amadeus-logo.png" alt="Amadeus Logo" className="h-6" />
-      <ul className="flex space-x-3 text-xl text-[#FFFFFF] gap-8">
-        <li>
-          <Link to="/">Inicio</Link>
-        </li>
+    <nav
+      className={`backdrop-blur-lg bg-gradient-to-t from-sky-600/20 to-sky-300/5 text-white px-[2rem] py-[1.1rem] top-4 flex justify-between items-center fixed transition-opacity duration-500 ease-in-out mx-4 md:mx-8 rounded-xl w-[calc(100%-2rem)] md:w-[calc(100%-4rem)] z-50 ${navbarOpacity}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Link to={"/"} className="flex items-center space-x-2">
+        <img src="/amadeus-logo.png" alt="Amadeus Logo" className="h-6" />
+      </Link>
+      <ul className="flex space-x-3 text-lg text-[#FFFFFF] gap-8">
+        {!isHomePage && (
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+        )}
         <li>
           <Link to="/contacto">Contacto</Link>
         </li>
